11e6cd6e8503bf07c30268408dca37e1f4fd7948
Mohammad Ghorab <mody.moustafa@yahoo.com>
2021-02-28 00:54:54 +0100

    No dashboard for students

--- app/controllers/dashboard/pages_controller.rb
+++ app/controllers/dashboard/pages_controller.rb
@@ -1,8 +1,18 @@
 module Dashboard
   class PagesController < Dashboard::DashboardController
+    before_action :current_user_is_student?
+
     def home
       @students = User.all.where(student_role: true).count
       @classrooms = Room.all.where(school_id: current_user.school.id).count
+    end
+
+    private
+
+    def current_user_is_student?
+      if current_user.student_role?
+        redirect_to dashboard_exams_path
+      end
     end
   end
 end
