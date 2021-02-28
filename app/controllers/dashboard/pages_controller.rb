94d7f8c31a158c4b802894ed2f029b66b9850330
Mohammad Ghorab <mody.moustafa@yahoo.com>
2021-02-28 02:17:24 +0100

    Remove classrooms counter from dashboard

--- app/controllers/dashboard/pages_controller.rb
+++ app/controllers/dashboard/pages_controller.rb
@@ -4,7 +4,7 @@ module Dashboard
 
     def home
       @students = User.all.where(student_role: true).count
-      @classrooms = Room.all.where(school_id: current_user.school.id).count
+      # @classrooms = Room.all.where(school_id: current_user.school.id).count
     end
 
     private
